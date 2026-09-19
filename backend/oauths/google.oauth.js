b85c975e2ee016641bc0ac26938563ca85988f21
satyamluci2407 <60713555+satyamluci2407@users.noreply.github.com>
2026-09-19 12:59:39 +0530

    Add dotenv support for Google OAuth callback URL

--- backend/oauths/google.oauth.js
+++ backend/oauths/google.oauth.js
@@ -1,13 +1,14 @@
 const passport = require("passport");
 
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+require("dotenv").config();
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "${process.env.BACKEND_URL}/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
       done(null, profile);
